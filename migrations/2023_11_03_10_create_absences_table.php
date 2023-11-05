8ac00057393f191392972a74fdd68d1dc7be5ce4
AyoubMrani <126605010+AyoubMrani@users.noreply.github.com>
2023-11-05 12:24:36 +0100

    Add files via upload

---
+++ migrations/2023_11_03_10_create_absences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('etudiant_id')->constrained();
+            $table->foreignId('module_id')->constrained();
+            $table->dateTime('DateDebut');
+            $table->dateTime('DateFin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absences');
+    }
+};
